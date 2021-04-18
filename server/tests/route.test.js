69ac85a37303644825c55e647f5e273ec081fc01
khanshakeeb <khanshakeeb@msn.com>
2021-04-18 03:40:52 +0200

    test

--- server/tests/route.test.js
+++ server/tests/route.test.js
@@ -1,9 +1,26 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-
+import database from '../src/lib/database';
 import app from '../src/app';
 
 chai.use(chaiHttp);
 chai.should();
 
+before(function (done) {
+    database.connect().then(()=> done()).catch((error)=> done(error));
+});
+
+after(function(done ){
+    database.disconnect().then(()=> done()).catch((error)=> done(error));
+});
+
+describe('Check if server is running', ()=>{
+    it('Ok,check if route has been initialized', (done)=>{
+        chai.request(app).get('/')
+            .end((error,response)=>{
+                response.should.have.status(200);
+                done();
+            });
+    });
+});
 
